0d013aaf39c7a594b35709af6fcdc00d6be63cdb
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-11-08 11:44:48 +0000

    updated lambda/data.py.

--- lambda/data.py
+++ lambda/data.py
@@ -33,12 +33,7 @@ SELECT ?posLabel ?addr ?loc WHERE {{
   FILTER(?rand < 0.30) .
 }} LIMIT {}"""
 
-Q_WINE="""PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX schema: <http://schema.org/>
-PREFIX : <http://noi.example.org/ontology/odh#>
-
-SELECT ?name ?vintage WHERE {
+Q_WINE="""SELECT ?name ?vintage WHERE {
   ?wine a :Wine ; :wineVintageYear ?vintage ; rdfs:label ?name ; :receivesWineAward ?aw.
   BIND(RAND() AS ?rand) .
   FILTER(?rand <= 0.10) .
