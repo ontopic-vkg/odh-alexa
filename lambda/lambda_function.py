6c8db50bd19c69d8cd7e4b528bab6be1b897ef1c
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-11-12 13:55:54 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -114,17 +114,15 @@ class LodgingSearchIntentHandler(AbstractRequestHandler):
             elif (len(results["results"]["bindings"]) == 1):
                 final_speech += " I found something. "
                 for result in results["results"]["bindings"]:
-                    final_speech += "The " + user_ltype + " is called <lang xml:lang='de-DE'>" + str(
-                        result["posLabel"]["value"]) + "</lang>. It's located in <lang xml:lang='it-IT'>" \
+                    final_speech += "The " + user_ltype + " is called <lang xml:lang='de-DE'>" + \
+                                    str(result["posLabel"]["value"]) + "</lang> and it's located in <lang xml:lang='it-IT'>" \
                                     + str(result["addr"]["value"]) + " " + str(result["loc"]["value"]) + "</lang>. "
             else:
                 final_speech += " I found " + str(len(results["results"]["bindings"])) + ". "
                 for count, result in enumerate(results["results"]["bindings"]):
                     final_speech += "Hotel number " + str(count + 1) + " is called <lang xml:lang='de-DE'>" + str(
-                        result["posLabel"]["value"]) \
-                                    + "</lang>. It's located in <lang xml:lang='it-IT'>" + str(
-                        result["addr"]["value"]) + " " + str(
-                        result["loc"]["value"]) + "</lang>. "
+                        result["posLabel"]["value"]) + "</lang> and it's located in <lang xml:lang='it-IT'>" \
+                        + str(result["addr"]["value"]) + " " + str(result["loc"]["value"]) + "</lang>. "
         except Exception:
             handler_input.response_builder.speak("There was a problem with the service request. ")
             return handler_input.response_builder.response
