2d2cd8d21380d13b7c1b602b28b0e4df43dd4dc7
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-11-12 14:26:56 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -79,6 +79,7 @@ class LodgingSearchIntentHandler(AbstractRequestHandler):
         # Example of loggin that can be useful for debugging
         # logger.info("Gave city value " + city)
         # logger.info(" Gave lodging_type value " + lodging_type)
+        
         final_speech = ""
         query_string = ""
         hotel_name = ""
