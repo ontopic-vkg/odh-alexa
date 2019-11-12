468cc840598d71cdefdc3fd4fdb7fc3481f99016
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-11-12 14:21:03 +0000

    updated lambda/data.py.

--- lambda/data.py
+++ lambda/data.py
@@ -2,7 +2,7 @@
 
 SKILL_NAME = "ODH"
 WELCOME = ["Welcome to the Open Data Hub!", "Hello and Welcome!", "This is the Open Data Hub speaking!"]
-HELP = "Say about to hear more about the open data hub or you can just ask me about lodging. Say exit to stop this interaction and quit the skill."
+HELP = "Say about to hear more about the open data hub or you just ask me about lodging or wine. Say exit to stop this interaction and quit the skill."
 ABOUT = "Open Data Hub is your access point to South Tyrol’s relevant data. The data is updated on a regular basis and easily accessible."
 STOP = "Okay, see you next time!"
 FALLBACK = "Hmmm I can't help you with that. I can help you learn about the open data hub or discover hotels though. Wanna try?"
