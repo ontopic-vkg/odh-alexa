e0e0cfecde2c67cc0a883734d1543e9ac9b05310
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-11-13 10:41:09 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -23,7 +23,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 # sparlq endpoint
-sparql_endpoint = SPARQLWrapper("https://sparql.opendatahub.testingmachine.eu/sparql")
+sparql_endpoint = SPARQLWrapper("https://80776885.ngrok.io")
 
 
 class LaunchRequestHandler(AbstractRequestHandler):
