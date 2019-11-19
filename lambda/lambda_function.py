690964dd846fe5bfa0baf675dac495050936ffb5
Geri Skenderi <geri.skenderi@ontopic.biz>
2019-11-19 12:27:57 +0100

    deploy demo commit

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -24,7 +24,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 # sparlq endpoint
-sparql_endpoint = SPARQLWrapper("https://99f4d3e7.ngrok.io/sparql")
+sparql_endpoint = SPARQLWrapper("https://99f4d3e7.ngrok.io/")
 
 
 class LaunchRequestHandler(AbstractRequestHandler):
