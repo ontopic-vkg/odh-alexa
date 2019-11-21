af0015fea95be088a78be0ff73a688b0a90e09d2
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-11-21 16:33:53 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -9,7 +9,6 @@ import ask_sdk_core.utils as ask_utils
 # from imports
 from ask_sdk_core.skill_builder import SkillBuilder
 from ask_sdk_core.dispatch_components import AbstractRequestHandler, AbstractExceptionHandler, AbstractResponseInterceptor, AbstractRequestInterceptor
-from ask_sdk_core.dispatch_components import 
 from ask_sdk_core.handler_input import HandlerInput
 from ask_sdk_model import ui, Response
 from ask_sdk_model.ui import SimpleCard
