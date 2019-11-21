dd679e867059403d65218778dd0d12361864ed52
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-11-21 15:05:01 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -169,7 +169,8 @@ class YesMoreLodgingInfoIntentHandler(AbstractRequestHandler):
         else:
             response_builder.set_card(SimpleCard(title=data.SKILL_NAME, content=card_info)).set_should_end_session(True)
 
-        return response_builder.speak(final_speech).response
+        response_builder.speak(final_speech)
+        return response_builder.response
 
         #handler_input.response_builder.speak(final_speech).set_card(
         #    SimpleCard(
