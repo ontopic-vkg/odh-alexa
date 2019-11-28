ad7c2dcbb22ffa7a81da174e6d58e1beac26d314
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-11-28 14:14:53 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -359,8 +359,7 @@ class HelpIntentHandler(AbstractRequestHandler):
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
-                logger.info("Improvement log: User called ThankIntent")
-
+        logger.info("Improvement log: User called ThankIntent")
         speak_output = data.HELP
 
         return (
