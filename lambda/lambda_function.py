efd9fe76a52b591f8146bd1fefe3b94ccb4301d4
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-11-29 11:16:51 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -100,7 +100,7 @@ class LodgingSearchIntentHandler(AbstractRequestHandler):
                 handler_input.response_builder.speak(final_speech)
                 return handler_input.response_builder.response
             else:
-                final_speech += " I found " + nr_lodgings["nrEstablishments"]["value"] + " in total. Here are some suggestions: "
+                final_speech += " I found " + nr_foode["nrEstablishments"]["value"] + " in total. Here are some suggestions: "
                 #for count, result in enumerate(lodging_results["results"]["bindings"]):
                 #    lodging_name = str(result["posLabel"]["value"])
                 #    lodging_address = str(result["addr"]["value"]) + " " + str(result["loc"]["value"])
