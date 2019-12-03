b2cdb4b3edd3c9fe689c861f7609e6585ed5ec67
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-12-03 15:41:13 +0000

    updated lambda/data.py.

--- lambda/data.py
+++ lambda/data.py
@@ -69,7 +69,7 @@ Q_RANDOM_FOODE_CITY = """PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX schema: <http://schema.org/>
 
-SELECT ?r ?posLabel ?addr WHERE {{
+SELECT ?posLabel ?addr ?loc ?phone WHERE {{
   ?r a schema:{} ; schema:name ?posLabel ; schema:address ?a .
   ?a schema:streetAddress ?addr ; schema:addressLocality ?loc .
   FILTER (lang(?posLabel) = 'de' && lang(?addr) = 'it' && lcase(?loc) = lcase('{}'@it)) .
