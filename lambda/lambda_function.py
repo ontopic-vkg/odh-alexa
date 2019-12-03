fa464a2565cf798bf0ec4401fb20e361e7a5dce3
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-12-03 15:34:53 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -259,7 +259,7 @@ class GetMoreInfoForFoodIntentHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
         session_attr = handler_input.attributes_manager.session_attributes
-        return (is_intent_name("GetMoreInfoForFoodIntent")(handler_input) and "foode_detail_list" in session_attr)
+        return (is_intent_name("GetMoreInfoForNumber")(handler_input) and "foode_detail_list" in session_attr)
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
